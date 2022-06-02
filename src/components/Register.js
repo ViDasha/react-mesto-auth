3fe66addb00d19bee001020d5cb1e102c5d5b527
Daria Vinogradova <vinodarya@yandex.ru>
2022-06-02 23:52:41 +0300

    feat: add Auth, change REgister

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,80 +1,59 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import * as auth from './Auth.js';
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
       email: '',
-      password: '',
-      confirmPassword: '',
-      calGoal: ''
+      password: ''
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleChange = (e) => {
     const {name, value} = e.target;
     this.setState({
       [name]: value
     });
   }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.password === this.state.confirmPassword){
-      /*auth.register(this.state.username, this.state.password, this.state.email)
-      .then((res) => {
-        if(res){
-          this.setState({
-            message: ''
-          }, () => {
-            this.props.history.push('/login');
-          })
-        } else {
-          this.setState({
-            message: 'Что-то пошло не так!'
-          })
-        }
-      });*/
-    }
+    auth.register(this.state.password, this.state.email)
+    .then((res) => {
+      if(res){
+        this.setState({
+          message: ''
+        }, () => {
+          this.props.history.push('/signin');
+        })
+      } else {
+        this.setState({
+          message: 'Что-то пошло не так!'
+        })
+      }
+    });
   }
+
   render(){
     return (
       <div className="register">
-        <p className="register__welcome">
-            Пожалуйста, зарегистрируйтесь.
-        </p>
-        <form onSubmit={this.handleSubmit} className="register__form">
-          <label htmlFor="username">
-            Логин:
-          </label>
-          <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleChange} />
-          <label htmlFor="email">
-            Email:
-          </label>
-          <input id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-          <label htmlFor="password">
-            Пароль:
-          </label>
-          <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleChange} />
-          <label htmlFor="confirmPassword">
-            Повторите пароль:
-          </label>
-          <input id="confirmPassword" name="confirmPassword" type="password" value={this.state.confirmPassword} onChange={this.handleChange} />
-          <label htmlFor="calGoal">
-            Калории за день:
-          </label>
-          <input id="calGoal" name="calGoal" type="number" value={this.state.calGoal} onChange={this.handleChange} />
-          <div className="register__button-container">
-            <button type="submit" onSubmit={this.handleSubmit} className="register__link">Зарегистрироваться</button>
-          </div>
+        <h2 className="popup__title popup__title_theme_dark">Регистрация</h2>
+        <form method="post" action="#" id="register-form" name="popup-form" className="popup__form" noValidate onSubmit={this.handleSubmit}>
+          <input id="email" name="email" type="email" className="popup__item" placeholder="E-mail" minLength="2" maxLength="40" required value={this.state.email} onChange={this.handleChange} />
+          <span className="email-error popup__item-error"></span>
+          <input id="password" name="password" type="password" className="popup__item" placeholder="Пароль" minLength="2" maxLength="40" required value={this.state.password} onChange={this.handleChange} />
+          <span className="password-error popup__item-error"></span>
+          <button type="submit" aria-label="Зарегистрироваться" name="save" className="popup__button-save" onSubmit={this.handleSubmit}>Зарегистрироваться</button>
         </form>
         <div className="register__signin">
           <p>Уже зарегистрированы?</p>
           <Link to="login" className="register__login-link">Войти</Link>
         </div>
-        </div>
+      </div>
   );
   }
 
